e944df7a91f86c8fe20e75816a2dc64626f69646
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-03-21 03:26:50 +0300

    Testte alınan hatalar düzeltilmeye çalışılıyor. v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,15 @@ public class Main {
         Person person1 = new Person("John","Doe",20,"Male","Backend Developer","NY City");
         Person person2 = new Person("Jane","Doe",17,"Boston","Student");
         Person person3 = new Person ("Burak",27);
+        Person person4 = new Person("Ali", 37);
         System.out.println("Firstname" + " " + person1.getFirstName());
         System.out.println("Lastname" + " " + person1.getLastName());
         System.out.println("Age" + " " + person1.getAge());
-        System.out.println(Person.isTeen(person2.getFirstName(), person2.getAge()));
-        System.out.println(Person.isTeen("Ali",37));
+        System.out.println(person1.isTeen(person1.getFirstName(), person1.getAge()));
+        System.out.println(person2.isTeen(person2.getFirstName(), person2.getAge()));
+        System.out.println(person3.isTeen(person3.getFirstName(), person3.getAge()));
+        System.out.println(person4.isTeen(person4.getFirstName(), person4.getAge()));
+
         //
         Wall wall = new Wall(5,4);
 
